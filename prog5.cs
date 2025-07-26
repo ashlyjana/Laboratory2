9d3547667aff48a2756aa6ce2a39d8e2440fa644
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-07-26 22:30:29 +0800

    Add files via upload

---
+++ prog5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program to shift elements of an array by k positions
+            int[] arr = new int[10];
+            int[] shifted = new int[10];
+
+            // Input: 10 integers
+            Console.WriteLine("Enter 10 integers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Input: value of k
+            Console.Write("\nEnter value of k (shifts): ");
+            int k = int.Parse(Console.ReadLine());
+
+            // Handle k > array length
+            k = k % 10;
+
+            for (int i = 0; i < 10; i++)//for loop
+            {
+                shifted[(i + k) % 10] = arr[i];
+            }
+
+            // Display original array
+            Console.WriteLine("\nOriginal Array:");
+            foreach (int num in arr)
+                Console.Write(num + " ");
+
+            // Display shifted array
+            Console.WriteLine("\nShifted Array:");
+            foreach (int num in shifted)
+                Console.Write(num + " ");
+        }
+    }
+}
---
+++ prog6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program reads 15 names from the user, stores them in an array,
+            string[] names = new string[15];
+
+            Console.WriteLine("Enter 15 names:");
+            for (int i = 0; i < 15; i++)//for loop to read names
+                names[i] = Console.ReadLine().ToLower(); 
+            
+            var result = names.Distinct().OrderBy(name => name).ToArray();
+            
+            Console.WriteLine("\nUnique, Sorted Names:");
+            for (int i = 0; i < result.Length; i++) //for loop to print each name
+                Console.WriteLine(result[i]);
+
+        }
+    }
+}
