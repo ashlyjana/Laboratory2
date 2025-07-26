9452dbe333d49e39151083ab4821a71e85b2f85c
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-07-26 22:18:02 +0800

    Update prog9.cs

--- prog9.cs
+++ prog9.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace prog9 
+    
+internal class Program
 {
     static void Main(string[] args)
     {
